bc6761e044771a7704a8bb30e5314db94dbd9cfe
alsfkl222 <alskfl222@kakao.com>
2022-08-22 19:56:52 +0900

    [ FEAT ] : init

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <div>
+      <h1>HOME</h1>
+      <ul>
+        <li>
+          <Link href='about'>ABOUT</Link>
+        </li>
+        <li><Link href='post'>POST</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
